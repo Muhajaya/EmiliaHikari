257cefe06a34fc8704ae863ba8641a48f1c459d8
Ayra Hikari <tedyramadhani2@gmail.com>
2018-08-17 12:29:44 +0000

    Fix an AFK bug

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -40,8 +40,8 @@ def no_longer_afk(bot: Bot, update: Update):
 @run_async
 def reply_afk(bot: Bot, update: Update):
     message = update.effective_message  # type: Optional[Message]
-    if message.entities and message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION]):
-        entities = message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+    entities = message.parse_entities([MessageEntity.TEXT_MENTION, MessageEntity.MENTION])
+    if message.entities and entities:
         for ent in entities:
             if ent.type == MessageEntity.TEXT_MENTION:
                 user_id = ent.user.id
@@ -59,12 +59,12 @@ def reply_afk(bot: Bot, update: Update):
                 return
 
             if sql.is_afk(user_id):
-                user = sql.check_afk_status(user_id)
-                if not user.reason:
-                    res = "{} sedang AFK!".format(fst_name)
-                else:
-                    res = "{} sedang AFK!\nKarena : {}".format(fst_name, user.reason)
-                message.reply_text(res)
+                valid, reason = sql.check_afk_status(user_id)
+                if valid:
+                    if not reason:
+                        res = "{} sedang AFK!".format(fst_name)
+                    else:
+                        res = "{} sedang AFK!\nKarena : {}".format(fst_name, user.reason)
             
     #        check_afk(bot, update, user_id, fst_name)
 
