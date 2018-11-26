9b82f81c52f6b4e274de07e97099cc9523fd6cf3
Ayra Hikari <AyraHikari@gmail.com>
2018-11-27 05:05:40 +0700

    Beta branch
    Signed-off-by: Ayra Hikari <AyraHikari@gmail.com>

---
+++ emilia/modules/connection.py
@@ -0,0 +1,122 @@
+from typing import Optional, List
+
+from telegram import ParseMode
+from telegram import Message, Chat, Update, Bot, User
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, Filters
+from telegram.ext.dispatcher import run_async
+from telegram.utils.helpers import mention_html
+
+import emilia.modules.sql.connection_sql as sql
+from emilia import dispatcher, LOGGER, SUDO_USERS
+from emilia.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
+from emilia.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from emilia.modules.helper_funcs.string_handling import extract_time
+
+@user_admin
+@run_async
+def allow_connections(bot: Bot, update: Update, args: List[str]) -> str:
+    chat = update.effective_chat  # type: Optional[Chat]
+    if chat.type != chat.PRIVATE:
+        if len(args) >= 1:
+            var = args[0]
+            if (var == "no" or var == "tidak"):
+                sql.set_allow_connect_to_chat(chat.id, False)
+                update.effective_message.reply_text("Sambungan yang dinonaktifkan ke obrolan ini untuk pengguna")
+            elif(var == "yes" or var == "ya"):
+                sql.set_allow_connect_to_chat(chat.id, True)
+                update.effective_message.reply_text("Mengaktifkan koneksi ke obrolan ini untuk pengguna")
+            else:
+                update.effective_message.reply_text("Silakan masukkan ya atau tidak!", parse_mode=ParseMode.MARKDOWN)
+        else:
+            update.effective_message.reply_text("Silakan masukkan ya atau tidak!", parse_mode=ParseMode.MARKDOWN)
+    else:
+        update.effective_message.reply_text("Silakan masukkan ya atau tidak di grup Anda!", parse_mode=ParseMode.MARKDOWN)
+
+
+@run_async
+def connect_chat(bot, update, args):
+    chat = update.effective_chat  # type: Optional[Chat]
+    user = update.effective_user  # type: Optional[User]
+    if update.effective_chat.type == 'private':
+        if len(args) >= 1:
+            try:
+                connect_chat = int(args[0])
+            except ValueError:
+                update.effective_message.reply_text(chat.id, "ID Obrolan tidak valid!")
+            if (bot.get_chat_member(connect_chat, update.effective_message.from_user.id).status in ('administrator', 'creator') or 
+                                     (sql.allow_connect_to_chat(connect_chat) == True) and 
+                                     bot.get_chat_member(connect_chat, update.effective_message.from_user.id).status in ('member')) or (
+                                     user.id in SUDO_USERS):
+
+                connection_status = sql.connect(update.effective_message.from_user.id, connect_chat)
+                if connection_status:
+                    chat_name = dispatcher.bot.getChat(connected(bot, update, chat, user.id, need_admin=False)).title
+                    update.effective_message.reply_text("Berhasil tersambung ke *{}*".format(chat_name), parse_mode=ParseMode.MARKDOWN)
+                else:
+                    update.effective_message.reply_text("Koneksi gagal!")
+            else:
+                update.effective_message.reply_text("Sambungan ke obrolan ini tidak diizinkan!")
+        else:
+            update.effective_message.reply_text("Tulis ID obrolan untuk terhubung!")
+
+    else:
+        update.effective_message.reply_text("Penggunaan terbatas hanya untuk PM!")
+
+
+def disconnect_chat(bot, update):
+    if update.effective_chat.type == 'private':
+        disconnection_status = sql.disconnect(update.effective_message.from_user.id)
+        if disconnection_status:
+           sql.disconnected_chat = update.effective_message.reply_text("Terputus dari obrolan!")
+        else:
+           update.effective_message.reply_text("Memutus sambungan tidak berhasil!")
+    else:
+        update.effective_message.reply_text("Penggunaan terbatas hanya untuk PM")
+
+
+def connected(bot, update, chat, user_id, need_admin=True):
+    if chat.type == chat.PRIVATE and sql.get_connected_chat(user_id):
+        conn_id = sql.get_connected_chat(user_id).chat_id
+        if (bot.get_chat_member(conn_id, user_id).status in ('administrator', 'creator') or 
+                                     (sql.allow_connect_to_chat(connect_chat) == True) and 
+                                     bot.get_chat_member(user_id, update.effective_message.from_user.id).status in ('member')) or (
+                                     user_id in SUDO_USERS):
+            if need_admin == True:
+                if bot.get_chat_member(conn_id, update.effective_message.from_user.id).status in ('administrator', 'creator') or user_id in SUDO_USERS:
+                    return conn_id
+                else:
+                    update.effective_message.reply_text("Anda harus menjadi admin dalam grup yang terhubung!")
+                    exit(1)
+            else:
+                return conn_id
+        else:
+            update.effective_message.reply_text("Grup mengubah koneksi hak atau Anda bukan admin lagi.\nSaya putuskan koneksi Anda.")
+            disconnect_chat(bot, update)
+            exit(1)
+    else:
+        return False
+
+
+
+__help__ = """
+Tindakan tersedia dengan grup yang terhubung:
+ • Lihat dan edit catatan
+ • Lihat dan edit filter
+ • Lihat dan edit blacklist
+ • Lebih banyak di masa kedepannya!
+
+ - /connect <chatid>: Hubungkan ke obrolan jarak jauh
+ - /disconnect: Putuskan sambungan dari obrolan
+ - /allowconnect on/yes/off/no: Izinkan menghubungkan pengguna ke grup
+"""
+
+__mod_name__ = "Koneksi"
+
+CONNECT_CHAT_HANDLER = CommandHandler("connect", connect_chat, allow_edited=True, pass_args=True)
+DISCONNECT_CHAT_HANDLER = CommandHandler("disconnect", disconnect_chat, allow_edited=True)
+ALLOW_CONNECTIONS_HANDLER = CommandHandler("allowconnect", allow_connections, allow_edited=True, pass_args=True)
+
+dispatcher.add_handler(CONNECT_CHAT_HANDLER)
+dispatcher.add_handler(DISCONNECT_CHAT_HANDLER)
+dispatcher.add_handler(ALLOW_CONNECTIONS_HANDLER)
---
+++ emilia/modules/rules.py
@@ -0,0 +1,112 @@
+from typing import Optional
+
+from telegram import Message, Update, Bot, User
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, run_async, Filters
+from telegram.utils.helpers import escape_markdown
+
+import emilia.modules.sql.rules_sql as sql
+from emilia import dispatcher
+from emilia.modules.helper_funcs.chat_status import user_admin
+from emilia.modules.helper_funcs.string_handling import markdown_parser
+
+
+@run_async
+def get_rules(bot: Bot, update: Update):
+    chat_id = update.effective_chat.id
+    send_rules(update, chat_id)
+
+
+# Do not async - not from a handler
+def send_rules(update, chat_id, from_pm=False):
+    bot = dispatcher.bot
+    user = update.effective_user  # type: Optional[User]
+    try:
+        chat = bot.get_chat(chat_id)
+    except BadRequest as excp:
+        if excp.message == "Chat not found" and from_pm:
+            bot.send_message(user.id, "Pintasan aturan untuk obrolan ini belum diatur dengan benar! Mintalah admin untuk "
+                                      "perbaiki ini.")
+            return
+        else:
+            raise
+
+    rules = sql.get_rules(chat_id)
+    text = "Peraturan untuk *{}* adalah:\n\n{}".format(escape_markdown(chat.title), rules)
+
+    if from_pm and rules:
+        bot.send_message(user.id, text, parse_mode=ParseMode.MARKDOWN)
+    elif from_pm:
+        bot.send_message(user.id, "Admin grup belum menetapkan aturan apa pun untuk obrolan ini. "
+                                  "Bukan berarti obrolan ini tanpa hukum...!")
+    elif rules:
+        update.effective_message.reply_text("Hubungi saya di PM untuk mendapatkan aturan grup ini",
+                                            reply_markup=InlineKeyboardMarkup(
+                                                [[InlineKeyboardButton(text="Peraturan",
+                                                                       url="t.me/{}?start={}".format(bot.username,
+                                                                                                     chat_id))]]))
+    else:
+        update.effective_message.reply_text("Admin grup belum menetapkan aturan apa pun untuk obrolan ini. "
+                                            "Bukan berarti obrolan ini tanpa hukum...!")
+
+
+@run_async
+@user_admin
+def set_rules(bot: Bot, update: Update):
+    chat_id = update.effective_chat.id
+    msg = update.effective_message  # type: Optional[Message]
+    raw_text = msg.text
+    args = raw_text.split(None, 1)  # use python's maxsplit to separate cmd and args
+    if len(args) == 2:
+        txt = args[1]
+        offset = len(txt) - len(raw_text)  # set correct offset relative to command
+        markdown_rules = markdown_parser(txt, entities=msg.parse_entities(), offset=offset)
+
+        sql.set_rules(chat_id, markdown_rules)
+        update.effective_message.reply_text("Berhasil mengatur aturan untuk grup ini.")
+
+
+@run_async
+@user_admin
+def clear_rules(bot: Bot, update: Update):
+    chat_id = update.effective_chat.id
+    sql.set_rules(chat_id, "")
+    update.effective_message.reply_text("Berhasil membersihkan aturan!")
+
+
+def __stats__():
+    return "{} obrolan memiliki aturan yang ditetapkan.".format(sql.num_chats())
+
+
+def __import_data__(chat_id, data):
+    # set chat rules
+    rules = data.get('info', {}).get('rules', "")
+    sql.set_rules(chat_id, rules)
+
+
+def __migrate__(old_chat_id, new_chat_id):
+    sql.migrate_chat(old_chat_id, new_chat_id)
+
+
+def __chat_settings__(chat_id, user_id):
+    return "Obrolan ini memiliki aturan yang ditetapkan: `{}`".format(bool(sql.get_rules(chat_id)))
+
+
+__help__ = """
+ - /rules: dapatkan aturan untuk obrolan ini.
+
+*Hanya admin:*
+ - /setrules <aturan Anda di sini>: atur aturan untuk obrolan ini.
+ - /clearrules: kosongkan aturan untuk obrolan ini.
+"""
+
+__mod_name__ = "Rules"
+
+GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
+SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
+RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, filters=Filters.group)
+
+dispatcher.add_handler(GET_RULES_HANDLER)
+dispatcher.add_handler(SET_RULES_HANDLER)
+dispatcher.add_handler(RESET_RULES_HANDLER)
